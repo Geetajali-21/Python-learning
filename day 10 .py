85c8fcd04aac11346ec459ab17b73103522ae2f1
Geetajali-21 <guptagitanjali7744@gmail.com>
2026-02-21 23:26:29 +0545

    Day 10 : fuctions

---
+++ day 10 .py
@@ -0,0 +1,15 @@
+def avg():
+   a=int(input("enter a nuber"))
+   b=int(input("enter a nuber"))
+   c=int(input("enter a nuber"))
+   average=(a+b+c)/3
+   print(average)
+
+avg()
+print("thanks")
+def greets():
+    print("good morning")
+
+greets()
+greets()
+greets()
