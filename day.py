b131303642ba05768351775c13ed382acbcf6ee9
Geetajali-21 <guptagitanjali7744@gmail.com>
2026-02-21 22:36:04 +0545

    Day 1 : operators and basic practice

---
+++ day.py
@@ -0,0 +1,29 @@
+#DAY 1
+a=11
+b=7
+c=a+b
+print(c)
+# assingment opretor
+a=4-2
+b=6
+# b+=3  #increment the value
+b -=2
+b *=3
+b /=3
+print(b)
+# comparision opretor
+d = 5<4
+print(d)
+a=3
+b="geet"
+
+t=type (d)
+print(t)
+a = int(input("enter "))
+b = int(input("enter b"))
+print("num is" ,a) 
+print("num b", b)
+
+print(a%b)
+print(a>b)
+print("average", a**2)
