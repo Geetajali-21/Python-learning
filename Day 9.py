57256f109dd53a3cd92b534dea26542f47aafaab
Geetajali-21 <guptagitanjali7744@gmail.com>
2026-02-21 23:23:50 +0545

    Day9 : loops , while and for

---
+++ Day 9.py
@@ -0,0 +1,54 @@
+i=0
+while i<6:
+    print("geeta")
+    i= i+1    
+l=[1,2,3,"geeta",False]
+i=0
+while i<len(l):
+    print(l[i])
+    i +=1
+  for i in range(1,15,3):
+     print(i)
+l=(1,2,3,4,5,"geeta")
+l[0]= 8
+print(l)
+s="gettanjali"
+for i in s:
+    print(i)
+for i in range(1,50):
+     if(i==22):
+       break
+       print(i)
+for i in range(1,50):
+   if(i==22):
+      continue
+print(i)
+for i in range(1,100):
+      print(i)
+pass
+n=int(input("enter nuber for table:"))
+i=1
+while i<11:
+  print(f"{n}x{i}={n*i}")   
+  i += 1     
+l = ["rAm ","shyam","sohan","khushi","faizal"]
+for i in l:
+  if(i.startswith("s")):
+        print(f"Nmaste {i}")
+for i in range(1,50):
+    if (i % 1==0 and i% 1==0 and i%):
+        n= int(input("enter a numbe")
+            i=1
+            sum =0
+
+while (i<=n):
+    sum +=i
+    i +=1
+    print(sum)
+fact=1
+for i in range(1,n+1):
+    fact = fact*i
+    print(fact)
+for i in range (1,n):
+    
+    print("*")
