95f24cd690d01c3b29cc98ce7fda80f687b0d44d
Geetajali-21 <guptagitanjali7744@gmail.com>
2026-02-21 22:50:08 +0545

    Day3 : list and opration

---
+++ Day3.py
@@ -0,0 +1,16 @@
+fruit=["apple","orange","rohan",5,6,7,3.45,False]
+fruit.append("geetanjli")
+print(fruit)
+
+l1=[1,2,3,4,5]
+l1.insert(3, 66)
+print(l1)
+l1.reverse()
+print(l1)
+l1.sort()
+print(l1)
+l1.pop(4)
+print(l1)
+print(type(fruit[2]))
+print(l1.count())
+
